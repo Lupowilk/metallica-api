11ad6d6c6070d605d891dd376c73679019b37612
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-04-28 09:10:52 +0200

    Implement SongDatabase with sample Metallica songs

--- src/data.rs
+++ src/data.rs
@@ -7,12 +7,69 @@ pub struct SongDatabase {
 
 impl SongDatabase {
     // Create a new empty database
-    pub fn new() -> Self {
+    pub fn new_db() -> Self {
         SongDatabase {
             songs: Mutex::new(Vec::new()),
         }
     }
 
     // Initialize with sample data
+    pub fn init_sample_db() -> Self {
+        let mut db = SongDatabase::new_db();
+        
+        let song1 = Song {
+            title: String::from("Enter Sandman"),
+            album: String::from("Metallica"),
+            year: 1991,
+            length_seconds: 332,
+        };
+        
 
+        let song2 = Song {
+            title: String::from("Inamorata"),
+            album: String::from("72 Seasons"),
+            year: 2022,
+            length_seconds: 521,
+        };
+
+        let song3 = Song {
+            title: String::from("Master of Puppets"),
+            album: String::from("Master of Puppets"),
+            year: 1986,
+            length_seconds: 515,
+        };
+        
+        let song4 = Song {
+            title: String::from("One"),
+            album: String::from("...And Justice for All"),
+            year: 1988,
+            length_seconds: 447,
+        };
+
+        let song5 = Song {
+            title: String::from("Nothing Else Matters"),
+            album: String::from("Metallica"),
+            year: 1991,
+            length_seconds: 386,
+        };
+
+        let song6 = Song {
+            title: String::from("For Whom the Bell Tolls"),
+            album: String::from("Ride the Lightning"),
+            year: 1984,
+            length_seconds: 310,
+        };
+        
+        {
+        let mut songs_lock = db.songs.lock().unwrap();
+            songs_lock.push(song1);
+            songs_lock.push(song2);
+            songs_lock.push(song3);
+            songs_lock.push(song4);
+            songs_lock.push(song5);
+            songs_lock.push(song6);
+        }; // Release the song_lock borrow
+        
+        db
+    }
 }
