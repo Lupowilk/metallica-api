55e8af12e071f4f5d818988da30961b2297f4f16
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-05-08 10:55:03 +0200

    Add get_song handler function for retrievien all songs

--- src/routes.rs
+++ src/routes.rs
@@ -7,6 +7,7 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
 // Create Handler Functions
 
 #[get("/songs")]
-async fn get_songs(db: web::<SongDatabase>) -> impl Responder {
-    
+async fn get_songs(db: web::Data<SongDatabase>) -> impl Responder {
+    let songs = db.get_all_songs(); // retrieves the full list of songs from your database
+    HttpResponse::Ok().json(songs) // returns the list of songs as JSON
 }
