950a82fa8998f90da57cda2644c339f099217f6b
Lupowilk <105706387+Lupowilk@users.noreply.github.com>
2025-05-14 09:05:10 +0200

    Add proper imports for web server setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 mod model;
+mod routes;
+mod data;
+
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use crate::data::SongDatabase;
+use crate::routes::config_routes;
 
 fn main() {
     println!("Hello, world!");
